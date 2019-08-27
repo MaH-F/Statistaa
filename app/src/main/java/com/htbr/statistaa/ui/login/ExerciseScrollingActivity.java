151add753cb2f21d3f5131ef7bdf691a0e59341d
mh <ma@mh-Lenovo>
2019-08-27 19:14:34 +0200

    first release

--- app/src/main/java/com/htbr/statistaa/ui/login/ExerciseScrollingActivity.java
+++ app/src/main/java/com/htbr/statistaa/ui/login/ExerciseScrollingActivity.java
@@ -2,14 +2,13 @@ package com.htbr.statistaa.ui.login;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
-import android.view.View;
+
 import android.widget.TextView;
 
 import com.htbr.statistaa.R;
